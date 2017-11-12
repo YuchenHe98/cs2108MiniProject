8042876b95530388b1a0c50584c1fca1380879f0
Yuchen He <yuchenhe@Yuchens-MBP.workgroup>
2017-11-13 06:51:00 +0800

    update process.py

--- process.py
+++ process.py
@@ -1,9 +1,14 @@
 from collections import OrderedDict
 import face_recognition
 import os
+import sqlite3
+from sqlite3 import Error
 
-def get_matched_results():
-    all_pictures = os.listdir("profile_pictures/females")
+def get_matched_results(gender, description):
+    if gender == 'Female':
+        all_pictures = os.listdir("profile_pictures/females")
+    else:
+        all_pictures = os.listdir("profile_pictures/males")
     all_pictures.pop(0)
     
     #get the feature vector of the uploaded picture.
@@ -11,7 +16,11 @@ def get_matched_results():
     image_to_test_encoding = face_recognition.face_encodings(image_to_test)[0]
     pic_dict = {}
     for picture in all_pictures:
-        current = face_recognition.load_image_file("profile_pictures/females/"+picture)
+        if gender == 'Female':
+            current = face_recognition.load_image_file("profile_pictures/females/"+picture)
+        else:
+            current = face_recognition.load_image_file("profile_pictures/males/"+picture)
+
         encoding = face_recognition.face_encodings(current)
         
         #compute the face distance.
@@ -24,4 +33,61 @@ def get_matched_results():
     #extract top 10 indices
     sorted_dict = OrderedDict(sorted(pic_dict.items(), key=lambda x: x[1]))
     top_indices = list(sorted_dict.keys())[:10]
-    return top_indices
+    return get_text_match(gender, description, top_indices)
+
+def get_text_match(gender, description, top_indices):
+    database = "database.db"
+ 
+    # create a database connection
+    conn = create_connection(database)
+    with conn:
+        texts = []
+        if(gender = 'Male'):
+            for index in top_indices:
+                texts.append(select_male_texts(conn, index)[0][0])
+        else:
+            for index in top_indices:
+                texts.append(select_female_texts(conn, index)[0][0])
+        return sim(description, texts)
+    
+def get_text_match(gender, description, top_indices):
+            
+
+ 
+ 
+def create_connection(db_file):
+    """ create a database connection to the SQLite database
+        specified by the db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+ 
+    return None
+ 
+ 
+def select_male_texts(conn, index):
+
+    cur = conn.cursor()
+    cur.execute("SELECT description FROM profile WHERE profile_image_id = "+index+"and gender = male")
+ 
+    rows = cur.fetchall()
+    return rows
+ 
+ 
+def select_female_text(conn, index):
+
+    cur = conn.cursor()
+    cur.execute("SELECT description FROM profile WHERE profile_image_id = "+index+"and gender = female")
+ 
+    rows = cur.fetchall()
+    return rows
+ 
+ 
+ 
+ 
+
