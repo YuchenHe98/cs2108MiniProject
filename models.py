1a34ba9f3474cbf9eb807ae775dd98576e58ad77
Yuchen He <yuchenhe@Yuchens-MBP.workgroup>
2017-11-13 06:51:25 +0800

    change its property

--- models.py
+++ models.py
@@ -3,8 +3,8 @@ from datetime import datetime
 
 
 class Profile(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True)
+    ids = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
     gender = db.Column(db.String(1))
     age = db.Column(db.Integer())
     description = db.Column(db.String(300))
